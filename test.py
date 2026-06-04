a807026bbbfe7fb01137ba97581d31cd5811283b
Qirong Luo <qrluo0201@gmail.com>
2026-06-04 17:26:58 +0800

    Add some comments.

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ import numpy as np
 np.random.seed(0)
 
 if __name__ == "__main__":
+    # A simple test to check if numpy is working correctly.
+    # Now I work on MAIN branch which is not recommended.
     a = np.random.rand(5)
     print(a)
